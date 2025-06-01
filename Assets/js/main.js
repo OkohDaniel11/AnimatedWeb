00a6d5c254645102e7f6214b923afab5a75da00c
OkohDaniel11 <danielokoh09@yahoo.co.uk>
2025-06-01 13:51:03 +0100

    first commit

---
+++ Assets/js/main.js
@@ -0,0 +1,57 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const menuIcon = document.querySelector('.menuIc');
+    const closeIcon = document.querySelector('.closeIc');
+    const navContainer = document.querySelector('.navContainer');
+
+    /*Open Nav Bar */
+    menuIcon.addEventListener('click', () => {
+        navContainer.classList.add('active');
+    });
+
+    /*Close Nav Bar */
+    closeIcon.addEventListener('click', () => {
+        navContainer.classList.remove('active');
+    });
+
+    var typed = new Typed('#element', {
+        strings: ['Free', 'New', 'Fashion'], // Add more words here
+        typeSpeed: 150,
+        backSpeed: 100,
+        backDelay: 1000,
+        startDelay: 500,
+        cursorChar: '|',
+        loop: true,
+    });
+
+    var typed = new Typed('#second', {
+        strings: ['Free', 'New', 'Fashion'], // Add more words here
+        typeSpeed: 150,
+        backSpeed: 100,
+        backDelay: 1000,
+        startDelay: 500,
+        cursorChar: '|',
+        loop: true,
+    });
+
+    var typed = new Typed('#third', {
+        strings: ['Style', 'Fit', 'Vibe'], // Add more words here
+        typeSpeed: 150,
+        backSpeed: 100,
+        backDelay: 1000,
+        startDelay: 500,
+        cursorChar: '|',
+        loop: true,
+    });
+
+    var typed = new Typed('#fourth', {
+        strings: ['Expression', 'Stand Out', 'Me'], // Add more words here
+        typeSpeed: 150,
+        backSpeed: 100,
+        backDelay: 1000,
+        startDelay: 500,
+        cursorChar: '|',
+        loop: true,
+    });
+})
